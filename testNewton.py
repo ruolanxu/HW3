07f6c3b1c6b2a938b9e76a3c36176d05bafed13e
Effi Xu <ruolanxu@princeton.edu>
2012-10-25 16:45:01 -0400

    Added testQuadratic in newton.py
    Test solving quadratic equation with different initial values

--- testNewton.py
+++ testNewton.py
@@ -4,6 +4,7 @@ import newton
 import unittest
 import numpy as N
 import functions as F
+import math as m
 
 class TestNewton(unittest.TestCase):
     def testLinear(self):
@@ -14,16 +15,27 @@ class TestNewton(unittest.TestCase):
         
     def testSingleStep(self):
         # f(x) = a* x^2 + b * x + c
-        a, b, c = 2, 3, 5
+        a, b, c = 2, 3, -5
         def f(x):
             return a * x**2 + b * x + c
         x0 = 2.0
-        #dx = 0.0001
         for dx in [1, 1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6]:
             solver = newton.Newton(f, maxiter=2, dx=dx)
             x = solver.step(x0)
             self.assertAlmostEqual(x, x0-f(x0)/((f(x0+dx)-f(x0))/dx))
-    
+            
+    def testQuadratic(self):
+        # f(x) = a* x^2 + b * x + c
+        a, b, c = 2.0, 3.0, -5.0
+        def f(x):
+            return a * x**2 + b * x + c
+        solver = newton.Newton(f, maxiter=1000, dx=1e-3)
+        x0 = [2.0, -4.5]
+        x_sol = [((-1) * b + m.sqrt(b**2 - 4 * a * c)) / (2.0 * a), 
+                 ((-1) * b - m.sqrt(b**2 - 4 * a * c)) / (2.0 * a)]
+        for i in range(2):
+            x = solver.solve(x0[i])
+            self.assertAlmostEqual(x, x_sol[i])
 
 if __name__ == "__main__":
     unittest.main()
